bcd8a9c520bdcecf894fc5e0d2bf5b25295d56f0
Cesar Vargas <cavargas@umich.edu>
2026-04-23 14:31:13 -0400

    redirected base repo

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 // vite.config.js
 export default defineConfig({
-  base: '/579-vanilla/', // Change this to your repository name
+  base: '/winter-2026-react-deliverable/', // Change this to your repository name
   plugins: [react()],
 })
